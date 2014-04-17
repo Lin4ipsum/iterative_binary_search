89c8941d4966629837658fde5e739ce29adfb020
Lin4ipsum <lin.m.gonzalez@gmail.com>
2014-04-17 09:30:52 -0400

    add specs for string array

--- binary_search_spec.rb
+++ binary_search_spec.rb
@@ -39,4 +39,42 @@ describe Search do
       Search.binary_search(array, value).should == nil
     end
   end
+
+   context "binary_search with string array" do
+    it "returns a value if a value less than mid_index" do
+      array = ["a", "c", "e", "l", "s"]
+      value = "c"
+      Search.binary_search(array, value).should == "c"
+    end
+
+    it "returns a value if a value greater than mid_index" do
+      array = ["a", "c", "e", "l", "s"]
+      value = "s"
+      Search.binary_search(array, value).should == "s"
+    end
+
+    it "returns a value if a value equal to mid_index" do
+      array = ["b", "c", "e", "l", "s"]
+      value = "e"
+      Search.binary_search(array, value).should == "e"
+    end
+
+    it "returns nil if value is less than min_value" do
+      array = ["b", "c", "e", "l", "s"]
+      value = "a"
+      Search.binary_search(array, value).should == nil
+    end
+
+    it "returns nil if value is greater than max_value" do
+      array = ["b", "c", "e", "l", "s"]
+      value = "z"
+      Search.binary_search(array, value).should == nil
+    end
+
+    it "returns nil if value is not in the array" do
+      array = ["b", "c", "e", "l", "s"]
+      value = "d"
+      Search.binary_search(array, value).should == nil
+    end
+  end 
 end
